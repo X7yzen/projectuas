0e15a9caedf0a100969cc95bac658346142e8045
X7yzen <ahmad.riziq1839@smk.belajar.id>
2024-10-23 08:57:26 +0700

    ini baru

---
+++ galeri.php
@@ -0,0 +1,6 @@
+<div class="container-fluid px-4">
+                        <h1 class="mt-4">Galeri Foto</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Galeri Foto</li>
+                        </ol>
+                    </div>
