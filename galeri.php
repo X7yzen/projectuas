d42194693c973c60927295055bb9f2079704585e
X7yzen <ahmad.riziq1839@smk.belajar.id>
2024-10-24 09:41:18 +0700

    ini baru

--- galeri.php
+++ galeri.php
@@ -1,9 +1,10 @@
-<div class="container-fluid px-4">
+
+<div class="container-fluid">
                         <h1 class="mt-4">Galeri Foto</h1>
                         <ol class="breadcrumb mb-4">
                             <li class="breadcrumb-item active">Galeri Foto</li>
                         </ol>
-
+                        <a href="?page=galeri_tambah">+ Tambah Galeri</a>
                         <table class="table table-bordered">
                             <tr>
                               <th>Gambar</th>
@@ -13,25 +14,25 @@
                               <th>Tanggal</th>
                               <th>Aksi</th>
                             </tr>
-
                             <?php
-                                $query = mysqli_query($koneksi, "SELECT*FROM foto");
+                
+                                $query = mysqli_query($koneksi, "SELECT foto.*, album.namaalbum FROM foto left join album on album.album_id = foto.album_id");
                                 while($data = mysqli_fetch_array($query)) {
 
                                     ?>
                                     <tr>
-                                        <td><img src="gambar/<?php echo $data['$gambar']; ?>" alt="gambar"></td>
+                                        <td><img src="gambar/<?php echo $data['gambar']; ?>" alt="gambar"></td>
                                         <td><?php echo $data['judul']; ?></td>
                                         <td><?php echo $data['nama_album']; ?></td>
-                                        <td><?php echo $data['deskripsi']; ?></td>
+                                        <td><?php echo $data['deskripsi']; ?></td> 
                                         <td><?php echo $data['tanggal']; ?></td>
                                         <td>
-                                            <a href="?page=galeri_ubah&&id=<?php echo $data['id_foto']; ?>" class="btn btn-primary">Ubah</a>
-                                            <a href="?page=galeri_hapus&&id=<?php echo $data['id_foto']; ?>" class="btn btn-primary">Hapus</a>
+                                            <a href="?page=galeri_ubah&&id=<?php echo $data['foto_id']; ?>" class="btn btn-primary">Ubah</a>
+                                            <a href="?page=galeri_hapus&&id=<?php echo $data['foto_id']; ?>" class="btn btn-primary">Hapus</a>
                                         </td>
                                     </tr>
                                     <?php
                                 }
-                                ?>
-                            
+                                ?> 
+                                </table>
                     </div>
